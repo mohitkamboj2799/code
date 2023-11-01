a622e9fef5a852b2a3c971c7fe01f1009acda351
mohitkamboj2799 <133959924+mohitkamboj2799@users.noreply.github.com>
2023-11-01 10:22:42 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,6 @@ sudo yum update
 sudo yum install docker -y
 sudo systemctl status docker
 sudo systemctl start docker
-sudo docker docker push mohitkamboj2799/simple-python-flask-app
-docker run -d 8080:8080 mohitkamboj2799/simple-python-flask-app
-eccho "success"
+sudo docker push mohitkamboj2799/simple-python-flask-app
+sudo docker run -d 8080:8080 mohitkamboj2799/simple-python-flask-app
+echo "success"
